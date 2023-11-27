1784b2a28c490aee9bdfc008cb232b29b1ab0e99
Brian Phelan <brianphelan59@gmail.com>
2023-11-27 14:26:56 -0500

    Add config

--- config.ts
+++ config.ts
@@ -1,7 +1,33 @@
 export default {
     // LOGIN DETAILS
-    EMAIL: process.env.USERNAME,
-    PASSWORD: process.env.PASSWORD,
+    USERNAME: "",
+    PASSWORD: "",
+    MAX_POSTS: 8,
+    LOGIN_URL: "https://forums.golfwrx.com/login/",
+    SUBFORUM_URL: "https://forums.golfwrx.com/forum/56-classifieds-for-sale-forum/",
+    REPLIES: [
+        "Interested! If only my wife would let me buy another set...",
+        "Nice clubs! If I were good enough, I'd snatch them up. GLWS!",
+        "If I had the skills to match these clubs, I'd be all over them. Good luck with the sale!",
+        "Wish I could justify another set, but the wife's not on board. GLWS!",
+        "Great deal! If only my swing deserved these clubs. Best of luck selling!",
+        "These would be perfect if I were a better golfer. GLWS!",
+        "If my game matched the quality of these clubs, I'd be buying them. Best of luck with the sale!",
+        "Love the clubs! Unfortunately, my game isn't up to par. GLWS!",
+        "If only I had the excuse to buy another set! Good luck selling, though.",
+        "Nice clubs, but I'm not good enough to make the most of them. GLWS!",
+        "If only my wife was as passionate about golf as I am! Good luck selling these beauties. GLWS!",
+        "I'd grab these in a heartbeat if I had the skills to match. GLWS!",
+        "Wish I had the game for these clubs. Best of luck with the sale!",
+        "Great price! If only my game deserved these clubs. Good luck selling!",
+        "If my skills matched the quality of these clubs, I'd be all over them. GLWS!",
+        "Unfortunately, my wife isn't letting me add to the collection. GLWS!",
+        "Nice clubs! If only I were a better golfer. Best of luck with the sale!",
+        "Wish I had the excuse to pick these up. Good luck selling, and GLWS!",
+        "If only my swing could do justice to these clubs. Best of luck with the sale! GLWS!",
+        "Love the clubs, but my game isn't at that level. GLWS!",
+        "If only I had the skills to match the clubs, I'd be all over this. Good luck selling!"
+    ]
     
   }
   
