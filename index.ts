3c1db2bdf7d896b43435c318ce43b33eddac56e6
Brian Phelan <brianphelan59@gmail.com>
2023-11-27 12:56:47 -0500

    Add App State

--- index.ts
+++ index.ts
@@ -1,37 +1,70 @@
 import puppeteer, { Page } from "puppeteer";
 import config from "./config";
 import login from "./login";
+import navigateToTopic from "./navigateToTopic";
 
 interface AppState {
-  paused: boolean;
-}
-
-const wait = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
-
-const state: AppState = {
-  paused: false,
-};
-
+    paused: boolean;
+    post_count: number;
+  }
+  
+  const createAppState = (): {
+    getState: () => AppState;
+    setPaused: (value: boolean) => void;
+    incrementPostCount: () => void;
+  } => {
+    let paused = false;
+    let post_count = 0;
+  
+    const getState = (): AppState => ({
+      paused,
+      post_count,
+    });
+  
+    const setPaused = (value: boolean): void => {
+      paused = value;
+    };
+  
+    const incrementPostCount = (): void => {
+      post_count++;
+    };
+  
+    return {
+      getState,
+      setPaused,
+      incrementPostCount,
+    };
+  };
+  
+export const state = createAppState();
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    ignoreHTTPSErrors: true,
-    args: ["--disable-setuid-sandbox", "--no-sandbox",]
-  });
-  const context = await browser.createIncognitoBrowserContext();
-  const listingPage = await context.newPage();
+    const browser = await puppeteer.launch({
+        headless: false,
+        ignoreHTTPSErrors: true,
+        args: ["--disable-setuid-sandbox", "--no-sandbox",]
+    });
+    const context = await browser.createIncognitoBrowserContext();
+    const listingPage = await context.newPage();
 
-  const pages = await browser.pages();
+    const pages = await browser.pages();
 
-  await pages[0].close();
+    await pages[0].close();
 
-  await login({
-    page: listingPage,
-    email: config.EMAIL,
-    password: config.PASSWORD
-  });
+    await login({
+        page: listingPage,
+        username: config.USERNAME || "",
+        password: config.PASSWORD || ""
+    });
 
+    //await listingPage.goto(config.SUBFORUM_URL, { waitUntil: 'load' });
+
+  //while(state.getState().post_count < config.MAX_POSTS){
+
+    // await navigateToTopic({
+    //     page: listingPage
+    // })
+  //}
 
 
   // await browser.close();
