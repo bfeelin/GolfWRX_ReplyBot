10541d3e8fc5ee60f38c77083a77e7f80054a325
Brian Phelan <brianphelan59@gmail.com>
2023-11-27 13:39:31 -0500

    Check if user has posted on this topic, write the reply

--- navigateToTopic.ts
+++ navigateToTopic.ts
@@ -1,6 +1,7 @@
 import { Page } from 'puppeteer';
 import config from './config';
 import { state } from './index'
+import getRandomReply from './utils/getRandomReply';
 
 interface Params {
   page: Page;
@@ -19,17 +20,43 @@ async function getTopicList({ page }: Params): Promise<Array<string>> {
 
 };
 
+async function writeReply({ page }: Params): Promise<void> {
+    console.log('Writing reply...')
+    await page.waitForSelector('div[data-role="editorComposer"]');
+    await page.click('div[data-role="editorComposer"]');
+    await page.waitForSelector('.cke_wysiwyg_div');
+    await page.type('.cke_wysiwyg_div', getRandomReply());
+    await page.waitForSelector('button.ipsButton_primary');
+    await page.click('button.ipsButton_primary');
+}
 
 async function navigateToTopic({ page }: Params): Promise<void> {
-    let topicList = await getTopicList({ page })
+    let topicList = await getTopicList({ page });
     // Randomly select a topic
     const randomIndex = Math.floor(Math.random() * topicList.length);
     const randomTopic = topicList[randomIndex];
 
     // Output the selected topic
-    console.log('Navigating to randomly selected topic: ' + randomTopic)
-    await page.goto(randomTopic, {waitUntil: 'load'})
+    console.log('Navigating to randomly selected topic: ' + randomTopic);
+    await page.goto(randomTopic, { waitUntil: 'load' });
 
+    // Check if the page contains the username
+    const username = config.USERNAME;
+    const userHasPosted = await page.evaluate(() => {
+        const posts = Array.from(document.querySelectorAll('.post'));
+        for (const post of posts) {
+            const author = post.querySelector('.ipsUserPhoto')?.getAttribute('data-ipsusername');
+            if (author === username) {
+                return true;
+            }
+        }
+        return false;
+    });
+    if (userHasPosted) {
+        console.log('User has posted on this topic already. Skipping topic...');
+    } else {
+        writeReply({ page });
+    }
 }
 
 
