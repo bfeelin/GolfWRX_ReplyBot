ea383ab05aa6f5406a2a011ff139f8d944b30101
Brian Phelan <brianphelan59@gmail.com>
2023-11-28 14:33:37 -0500

    Handle getting topics thread content

---
+++ getThread.ts
@@ -0,0 +1,27 @@
+import { Page } from 'puppeteer';
+import wait from './utils/wait';
+
+interface Params {
+  page: Page;
+}
+
+async function getThread({ page }: Params): Promise<Array<string>> {
+  // Wait for the necessary content to load (you may need to adjust the waiting time)
+  await page.waitForSelector('div[data-role="commentContent"]');
+  await wait(2000)
+
+  // Extract the thread responses
+  const responses = await page.evaluate(() => {
+    const responseElements = document.querySelectorAll('div[data-role="commentContent"]');
+    const responses: string[] = [];
+
+    responseElements.forEach((element) => {
+      responses.push(element?.textContent?.trim() || '');
+    });
+
+    return responses;
+  });
+  return responses; 
+}
+
+export default getThread;
