d9868149ba007f7ec855afc1f190b4d3a0ebb12d
Brian Phelan <brianphelan59@gmail.com>
2023-11-29 11:23:44 -0500

    wait and scroll down before writing reply

--- src/writeReply.ts
+++ src/writeReply.ts
@@ -1,6 +1,7 @@
 import { Page } from 'puppeteer';
 import { state } from '../index'
 import wait from '../utils/wait';
+import scrollDown from '../utils/scrollDown'
 
 interface Params {
   page: Page;
@@ -8,10 +9,12 @@ interface Params {
 }
 
 async function writeReply({ page, reply }: Params): Promise<void> {
-    console.log('Writing reply...')
+    console.log('Writing reply...');
+    await wait(5000);
+    await scrollDown(page);
     await page.waitForSelector('div[data-role="editorComposer"]');
     await page.click('div[data-role="editorComposer"]');
-    await wait(2000)
+    await wait(5000)
     await page.waitForSelector('.cke_wysiwyg_div');
     await page.type('.cke_wysiwyg_div', reply);
     await page.waitForSelector('button.ipsButton_primary');
